08468bf56552c3c749c028077d57b7a8984c1d48
--unset <godfrey.luvandale@student.moringaschool.com>
2021-11-14 16:42:52 +0300

    add index.html

---
+++ script.js
@@ -0,0 +1,29 @@
+const  male = {
+    Sunday: "Kwasi",
+    Monday: "Kwadwo",
+    Tuesday: "Kwabena",
+    Wednesday: "Kwaku",
+    Thursday:  "Yaw",
+    Friday: "Kofi",
+    Saturday: "Kwame",
+  }
+  
+const female = {
+     Sunday: "Akosua",  
+     Monday: "Adwoa",
+     Tuesday: "Abenaa",
+     Wednesday: "Akua",
+     Thursday:  "Yaa",
+     Friday: "Afua",
+     Saturday: "Ama"
+   }
+
+    if(male){
+        const male="Sunday"
+    }
+   document.getElementById("smt").addEventListener("click", function () {
+      
+    alert("Your name is Kwasi")
+
+});
+
